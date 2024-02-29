c9748c4e612645563303d534c93683d7fb31be19
sinaziaee <sinaziaee99@gmail.com>
2024-02-28 18:26:11 -0700

    updated gitignore, so we have access to stocks historical datasets. Dashboard is updated

--- time_series/trading_view_dashboard.py
+++ time_series/trading_view_dashboard.py
@@ -2,8 +2,15 @@ import pandas as pd
 from lightweight_charts import Chart
 
 if __name__ == '__main__':
-    chart = Chart()
-
-    df = pd.read_csv('../datasets/stock_data/AAPL.csv')
-    chart.set(df)
-    chart.show()
+    
+    while True:
+        try:
+            symbol = input('Enter stock symbol: ')
+            if symbol == '-1':
+                break
+            df = pd.read_csv(f'../datasets/stock_data/{symbol}.csv')
+            chart = Chart()
+            chart.set(df)
+            chart.show(block=True)
+        except:
+            print('Invalid stock symbol. Please try again.')
